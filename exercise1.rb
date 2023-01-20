e2c7a5ae25d3857b36ab872a9daa573e81d44b17
divy-sys <divy.varia.1020@gmail.com>
2023-01-20 18:22:48 +0530

    Today's exercise

--- exercise1.rb
+++ exercise1.rb
@@ -1,8 +1,6 @@
-marks = {"Yash":23, "Abhi":40, "Sahil":88, "Rohan":60}
+  marks = {"Yash":23, "Abhi":40, "Sahil":88, "Rohan":60}
 
-def result(marks, passed_mark)
-  marks.group_by {|key, value| (value > passed_mark)? "Pass" : "fail"}
-end
-
-
-p result(marks, 40)
+  def result(marks, passed_mark)
+    marks.group_by {|key, value| (value > passed_mark)? "Pass" : "fail"}
+  end
+  p result(marks, 40)
